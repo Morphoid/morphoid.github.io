b5cb79346c705ea415820f00fa1245783659a776
morphoid <boss@morphoid.eu>
2016-03-06 20:24:21 +0100

    Sample commit

---
+++ console.js
@@ -0,0 +1,80 @@
+setTimeout(function(){
+        $(".element0").typed({
+            strings: ["ping morphoid.eu"],
+            typeSpeed: 30, // typing speed
+            backDelay: 150, // pause before backspacing
+            loop: false, // loop on or off (true or false)
+            loopCount: false, // number of loops, false = infinite
+            callback: function(){ } // call function after typing is done
+        });
+    }, 0);
+
+setTimeout(function(){
+        $(".element1").css("display", "inherit");
+        $(".element1").typed({
+            strings: [""],
+            typeSpeed: 30, // typing speed
+            backDelay: 55, // pause before backspacing
+            loop: false, // loop on or off (true or false)
+            loopCount: false, // number of loops, false = infinite
+            callback: function(){ } // call function after typing is done
+        });
+    }, 2000);
+
+setTimeout(function(){
+        $(".element2").css("display", "inherit");
+        $(".element2").typed({
+            strings: [""],
+            typeSpeed: 30, // typing speed
+            backDelay: 55, // pause before backspacing
+            loop: false, // loop on or off (true or false)
+            loopCount: false, // number of loops, false = infinite
+            callback: function(){ } // call function after typing is done
+        });
+    }, 4000);
+
+setTimeout(function(){
+        $(".element3").css("display", "inherit");
+        $(".element3").typed({
+            strings: [""],
+            typeSpeed: 30, // typing speed
+            backDelay: 55, // pause before backspacing
+            loop: false, // loop on or off (true or false)
+            loopCount: false, // number of loops, false = infinite
+            callback: function(){ } // call function after typing is done
+        });
+    }, 6000);
+
+setTimeout(function(){
+        $(".element4").css("display", "inherit");
+        $(".element4").typed({
+            strings: [""],
+            typeSpeed: 30, // typing speed
+            backDelay: 55, // pause before backspacing
+            loop: false, // loop on or off (true or false)
+            loopCount: false, // number of loops, false = infinite
+            callback: function(){ } // call function after typing is done
+        });
+    }, 8000);
+setTimeout(function(){
+        $(".element5").css("display", "inherit");
+        $(".element5").typed({
+            strings: [""],
+            typeSpeed: 30, // typing speed
+            backDelay: 55, // pause before backspacing
+            loop: false, // loop on or off (true or false)
+            loopCount: false, // number of loops, false = infinite
+            callback: function(){ } // call function after typing is done
+        });
+    }, 8000);
+    setTimeout(function(){
+        $(".element6").css("display", "inherit");
+        $(".element6").typed({
+            strings: [""],
+            typeSpeed: 30, // typing speed
+            backDelay: 55, // pause before backspacing
+            loop: false, // loop on or off (true or false)
+            loopCount: false, // number of loops, false = infinite
+            callback: function(){ } // call function after typing is done
+        });
+    }, 8000);
